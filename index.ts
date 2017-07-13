4db33cc3004d5a64bcb6d37d24a88912d937504d
Andrew Fong <id@andrewfong.com>
2017-07-12 17:50:20 -0700

    Test that interface is imported too

--- index.ts
+++ index.ts
@@ -5,3 +5,8 @@ export function test() {
   analytics.identify("my-uid");
   analytics.track("MY_EVENT", { prop: 123 });
 }
+
+// Interface should be visible too
+declare const notAnalytics: SegmentAnalytics.AnalyticsJS;
+notAnalytics.page("SOME_PAGE");
+
